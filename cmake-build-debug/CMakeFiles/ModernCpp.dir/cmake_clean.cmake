5ffb3e9d8deeee2f1c23574a3fd2e722158b604e
Adkhambek <mr.adkhambek@gmail.com>
2021-09-14 11:51:32 +0500

    today is second lesson

---
+++ cmake-build-debug/CMakeFiles/ModernCpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ModernCpp.dir/main.cpp.o"
+  "ModernCpp"
+  "ModernCpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ModernCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,17 @@
+// input
+// output
+// stream
+#include <iostream>
+
+using namespace std;
+
+/*
+
+ */
+int main() {
+
+    // print hi message
+    cout << " Hi";
+
+    return 0x0;
+}
