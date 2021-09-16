c08ef9011612cb83ed26d7f90c99ad0ef00eb88f
Adkhambek <mr.adkhambek@gmail.com>
2021-09-16 11:24:08 +0500

    Lesson 4

--- main.cpp
+++ main.cpp
@@ -9,10 +9,23 @@ using namespace std;
 
  */
 int main() {
+    int hour;
+    int minute;
+    int second;
 
-    // print hi message
-    cout << " Hi" << endl;
-    cout << "The end";
+    char flag;
+
+    // input data 12:20:20
+    cout << "Hour : ";
+    cin >> hour >> flag >> minute >> flag >> second;
+
+    // input data 12 20
+    int a;
+    int b;
+
+    cin >> a >> b;
+    cout << "\ta = " << a << endl;
+    cout << "\tb = "  << b << endl;
 
     return 0x0;
 }
